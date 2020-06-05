1c61a4d016256005c8ce555f9b6392e3a0c6d818
someone <someone@somewhere>
2020-06-05 00:47:14 -0300

    Versão inicial da página

---
+++ js/busca.js
@@ -0,0 +1,32 @@
+function buscar() {
+    let nome = document.getElementById("nome").value;
+    let nome2 = removerAcentos(nome.trim().replace(/\s\s+/g, ' ').toLowerCase());
+    if (nome2 !== "") {
+        let hash = hex_sha512(nome2);
+        if (hashes.has(hash)) {
+            encontrado();
+        } else {
+            naoencontrado();
+        }
+    }
+}
+
+function encontrado() {
+    let res = document.getElementById("resultado");
+    res.innerHTML = "O nome foi <b>encontrado</b> na lista. Como pode ser um homônimo, sugere-se buscar a lista completa e verificar se esta pessoa realmente está nela.";
+}
+
+function naoencontrado() {
+    let res = document.getElementById("resultado");
+    res.innerHTML = "O nome <b>não</b> foi encontrado.";
+}
+
+/**
+ * Remove acentos de strings
+ * @param  {String} string acentuada
+ * @return {String} string sem acento
+ */
+
+var map={"â":"a","Â":"A","à":"a","À":"A","á":"a","Á":"A","ã":"a","Ã":"A","ê":"e","Ê":"E","è":"e","È":"E","é":"e","É":"E","î":"i","Î":"I","ì":"i","Ì":"I","í":"i","Í":"I","õ":"o","Õ":"O","ô":"o","Ô":"O","ò":"o","Ò":"O","ó":"o","Ó":"O","ü":"u","Ü":"U","û":"u","Û":"U","ú":"u","Ú":"U","ù":"u","Ù":"U","ç":"c","Ç":"C"};
+
+function removerAcentos(s){ return s.replace(/[\W\[\] ]/g,function(a){return map[a]||a}) };
